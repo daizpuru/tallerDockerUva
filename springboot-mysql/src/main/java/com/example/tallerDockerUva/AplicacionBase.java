123d65b03e59358d17ebcfc31a84acffef50367b
daizpuru <daizpuru@gmail.com>
2019-10-17 11:40:24 +0200

    Aplicacion Java ejemplo

---
+++ springboot-mysql/src/main/java/com/example/tallerDockerUva/AplicacionBase.java
@@ -0,0 +1,12 @@
+package com.example.tallerDockerUva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacionBase {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AplicacionBase.class, args);
+    }
+}
